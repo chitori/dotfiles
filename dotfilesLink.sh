cdeeb008f22da05734296a19c0409ff1d5e8ac55
chitori <chitori.ukihoshi@gmail.com>
2020-03-17 18:47:44 +0900

    add toml

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
-ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/nvim/LanguageClient-neovim/ ~/.config/nvim/LanguageClient-neovim
+
+NVIM_DIR='~/.config/nvim'
+if [ ! -e ${NVIM_DIR} ]; then
+  mkdir $NVIM_DIR
+fi
+
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vimrc ${NVIM_DIR}/init.vim
+ln -sf ~/dotfiles/nvim/dein.toml ${NVIM_DIR}/dein.toml
+ln -sf ~/dotfiles/nvim/ftdetect ${NVIM_DIR}/ftdetect
+ln -sf ~/dotfiles/nvim/LanguageClient-neovim/ ${NVIM_DIR}/LanguageClient-neovim
+ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
+ln -sf ~/dotfiles/colors ${NVIM_DIR}/colors
+
